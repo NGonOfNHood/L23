6df73e84889a4e7b357593603e3e4a2dc1ee8c76
NGonOfNHood <brendenluu123@gmail.com>
2024-11-14 18:51:58 -0500

    changed frequency

--- sinwave.py
+++ sinwave.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_sine_wave():
+def plot_sine_wave(frequency=1):
     t = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * t)
+    y = np.sin(2 * np.pi * frequency * t)
     plt.plot(t, y)
     plt.show()
 
 if __name__ == "__main__":
-    plot_sine_wave()
+    plot_sine_wave(frequency=5)
